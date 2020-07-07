33770c7c734b5573cd26e58ff5f65062ab99f222
Jacob Grow <jacobagrow@gmail.com>
2020-07-07 13:41:16 -0600

    Added Book Model

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+namespace console_library.Models
+{
+  public class Book
+  {
+    public string Title { get; set;}
+    public string Author { get; set; }
+    public bool Available { get; set;
+    }
+  }
+}
